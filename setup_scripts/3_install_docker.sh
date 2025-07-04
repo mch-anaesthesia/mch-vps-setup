c4cc0ead9bc4c45235ba82f9dc7dd1223d5de3e4
Eamonn Upperton <uppertoe@gmail.com>
2025-07-04 10:19:01 +1000

    separate out docker install

--- setup_scripts/3_install_docker.sh
+++ setup_scripts/3_install_docker.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 echo "[+] Removing old Docker versions"
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove $pkg; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove -y $pkg; done
 
 echo "[+] Installing Docker Engine & Compose plugin"
 
 # Add Docker's official GPG key:
 apt-get update
-apt-get install ca-certificates curl
+apt-get install -y ca-certificates curl
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
@@ -20,7 +20,7 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Ensure the 'docker' group exists (won't error if it already does)
 if ! getent group docker >/dev/null; then
